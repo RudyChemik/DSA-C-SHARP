93c210d8c2d6aeb56e9c6eb90e904f4ae8f66ce7
RudyChemik <klefedronowelove@gmail.com>
2024-08-10 18:51:03 +0200

    BST

---
+++ DSA C-SHARP/DataStructures/Trees/BinarySearchTree (BST)/BSTNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_C_SHARP.DataStructures.Trees.BinarySearchTree__BST_
+{
+    public class BSTNode<T> where T : IComparable<T>
+    {
+        public T Key { get; set; }
+        public BSTNode<T>? Left { get; set; }
+        public BSTNode<T>? Right { get; set; }
+
+        public BSTNode(T key)
+        {
+            Key = key;
+            Left = null;
+            Right = null;
+        }
+    }
+}
